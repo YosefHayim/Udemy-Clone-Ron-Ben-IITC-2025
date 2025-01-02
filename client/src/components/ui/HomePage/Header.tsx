a6f17f464d73bda29789fcb8209117235d0e672c
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-02 09:37:31 +0200

    boker tov

--- client/src/components/ui/HomePage/Header.tsx
+++ client/src/components/ui/HomePage/Header.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import logo from '../../../assets/images/logo.png';
+import { ShoppingCartIcon, GlobeAltIcon } from "@heroicons/react/outline";
+
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between px-6 py-3 bg-white shadow-md">
+    <header className="flex items-center justify-between px-6 py-3 bg-white shadow-md w-screen">
       {/* Logo */}
       <div className="logo flex items-center gap-2">
-        <span className="text-purple-600 text-3xl font-bold">Udemy</span>
+        <img src={logo} alt="Udemy Logo" className="h-12 w-auto max-w-full align-middle" />
+
       </div>
 
       {/* Navegação */}
       <nav className="flex gap-6 text-sm text-gray-700">
         <a href="#" className="hover:text-black">Explore</a>
-        <a href="#" className="hover:text-black">Udemy Business</a>
-        <a href="#" className="hover:text-black">Teach on Udemy</a>
       </nav>
 
       {/* Barra de Busca */}
-      <div className="flex items-center border border-gray-300 rounded-full bg-gray-100 overflow-hidden px-4 py-2 w-1/3">
+      <div className="flex items-center border border-gray-300 rounded-full bg-gray-100 overflow-hidden px-4 py-2 w-1/2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="w-5 h-5 text-gray-500"
@@ -35,20 +37,20 @@ const Header = () => {
         />
       </div>
 
+      <a href="#" className="hover:text-black">Udemy Business</a>
+      <a href="#" className="hover:text-black">Teach on Udemy</a>
+
       {/* Ações */}
       <div className="flex items-center gap-4">
-        <button className="text-gray-700 hover:text-black">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path d="M6 3H18M6 3C4.34315 3 3 4.34315 3 6V18C3 19.6569 4.34315 21 6 21H18C19.6569 21 21 19.6569 21 18V6C21 4.34315 19.6569 3 18 3M6 3V1" />
-          </svg>
+        <button className="text-purple-600 hover:text-purple-800">
+          <ShoppingCartIcon className="w-6 h-6" />
         </button>
+
+        {/* Idioma */}
+        <button className="text-gray-700 hover:text-black">
+          <GlobeAltIcon className="w-6 h-6" />
+        </button>
+
         <a href="#" className="text-gray-700 text-sm hover:text-black">
           Log in
         </a>
