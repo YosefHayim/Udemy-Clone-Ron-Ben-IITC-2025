e649c8a0d44f5825dfec915bef9d2d9cf44790c0
ron959 <9592414@gmail.com>
2025-01-04 18:24:44 +0200

    updated

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -4,12 +4,13 @@ import Login from '../pages/LoginPage';
 import Signup from '../pages/SignupPage';
 import NotFound from '../pages/NotFound';
 import LeasonPage from '../pages/LeasonPage';
-
+import HomePage from '@/pages/HomePage';
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
         <Routes>
+        <Route path="/" element={<HomePage />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
