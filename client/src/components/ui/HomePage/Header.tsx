c3fa33258f9022420b16a6eaea3b6461a89a49fc
yosefhayim <yosefisabag+03@gmail.com>
2025-01-04 18:22:53 +0200

    a

--- client/src/components/ui/HomePage/Header.tsx
+++ client/src/components/ui/HomePage/Header.tsx
@@ -1,4 +1,4 @@
-import logo from '../../../assets/images/logo.png';
+// import logo from "../../../assets/images/logo.png";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { MdOutlineLanguage } from "react-icons/md";
 import { MdOutlineSearch } from "react-icons/md";
@@ -8,32 +8,59 @@ const Header = () => {
   const [isTyping, setIsTyping] = useState(false); // Estado para controlar se o usuário está digitando
 
   return (
-    <header className="flex items-center justify-between px-6 py-3 bg-white shadow-md w-screen" style={{ height: "4.5rem" }}>
+    <header
+      className="flex items-center justify-between px-6 py-3 bg-white shadow-md w-screen"
+      style={{ height: "4.5rem" }}
+    >
       {/* Logo */}
       <div className="logo flex items-center h-auto w-auto">
-        <img src={logo} alt="Udemy Logo" style={{ height: '2.65rem' }} className="w-auto max-w-full align-middle" />
+        <img
+          src={logo}
+          alt="Udemy Logo"
+          style={{ height: "2.65rem" }}
+          className="w-auto max-w-full align-middle"
+        />
       </div>
 
       {/* Navegação */}
       <nav>
-        <a href="#" className="text-[#020202] font-normal text-sm hover:text-purple-800 font-Sans">Explore</a>
+        <a
+          href="#"
+          className="text-[#020202] font-normal text-sm hover:text-purple-800 font-Sans"
+        >
+          Explore
+        </a>
       </nav>
 
       {/* Search bar */}
       <div className="flex items-center border border-gray-700 rounded-full overflow-hidden w-1/2 h-12 px-3 py-2 bg-gray-50">
         <MdOutlineSearch
-          className={`w-6 h-6 ${isTyping ? "text-gray-900" : "text-gray-400 opacity-200"}`}
+          className={`w-6 h-6 ${
+            isTyping ? "text-gray-900" : "text-gray-400 opacity-200"
+          }`}
         />
         <input
           type="text"
           placeholder="Search for anything"
           className="flex-1 bg-transparent text-gray-700 focus:outline-none text-sm ml-3 placeholder-gray-700 placeholder:text-sm  placeholder:font-Sans placeholder:font-normal bg-gray-50"
-          onInput={(e) => setIsTyping((e.target as HTMLInputElement).value !== "")} // Atualiza o estado baseado no texto digitado
+          onInput={(e) =>
+            setIsTyping((e.target as HTMLInputElement).value !== "")
+          } // Atualiza o estado baseado no texto digitado
         />
       </div>
 
-      <a href="#" className="text-[#020202] font-normal text-sm hover:text-purple-800 font-Sans">Udemy Business</a>
-      <a href="#" className="text-[#020202] font-normal text-sm hover:text-purple-800 font-Sans">Teach on Udemy</a>
+      <a
+        href="#"
+        className="text-[#020202] font-normal text-sm hover:text-purple-800 font-Sans"
+      >
+        Udemy Business
+      </a>
+      <a
+        href="#"
+        className="text-[#020202] font-normal text-sm hover:text-purple-800 font-Sans"
+      >
+        Teach on Udemy
+      </a>
 
       {/* Ações */}
       <div className="flex items-center gap-4">
@@ -41,10 +68,16 @@ const Header = () => {
           <MdOutlineShoppingCart className="w-6 h-6" />
         </button>
 
-        <a href="#" className="text-gray-700 text-sm px-4 py-2 font-bold hover:text-black hover:bg-gray-200 rounded border border-solid border-[#2d2f31] font-Sans">
+        <a
+          href="#"
+          className="text-gray-700 text-sm px-4 py-2 font-bold hover:text-black hover:bg-gray-200 rounded border border-solid border-[#2d2f31] font-Sans"
+        >
           Log in
         </a>
-        <a href="#" className="text-white bg-black font-bold px-4 py-2 rounded hover:opacity-70 transition duration-10 text-sm font-Sans">
+        <a
+          href="#"
+          className="text-white bg-black font-bold px-4 py-2 rounded hover:opacity-70 transition duration-10 text-sm font-Sans"
+        >
           Sign up
         </a>
 
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Header from '../components/ui/HomePage/Header';
-import Banner from '../components/ui/HomePage/Banner';
+import React from "react";
+import Header from "../components/ui/HomePage/Header";
+import Banner from "../components/ui/HomePage/Banner";
 // import Banner from '../components/homepage/Banner';
 // import Categories from '../components/homepage/Categories';
 // import CourseList from '../components/homepage/CourseList';
