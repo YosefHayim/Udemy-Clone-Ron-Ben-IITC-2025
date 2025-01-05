5799e3126c63f0325657f79f6bebc007e7115e79
yosefhayim <yosefisabag+03@gmail.com>
2025-01-05 15:07:42 +0200

    reomved all

--- client/src/utils/handsOnPractice.ts
+++
@@ -1,5 +0,0 @@
-export const handsOnPractice = [
-  { code: "quizzes", name: "Quizzes", count: 451 },
-  { code: "coding-exercises", name: "Coding Exercises", count: 94 },
-  { code: "practice-tests", name: "Practice Tests", count: 103 },
-];
--- client/src/utils/price.ts
+++
@@ -1,4 +0,0 @@
-export const prices = [
-  { code: "paid", name: "Paid", count: 791 },
-  { code: "free", name: "Free", count: 1275 },
-];
