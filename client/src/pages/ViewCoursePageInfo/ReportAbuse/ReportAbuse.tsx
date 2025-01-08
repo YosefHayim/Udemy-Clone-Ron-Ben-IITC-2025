fea0abda64e99d296a9471eb02c912267eb92608
yosefhayim <yosefisabag+03@gmail.com>
2025-01-08 15:15:45 +0200

    ds

--- client/src/pages/ViewCoursePageInfo/ReportAbuse/ReportAbuse.tsx
+++ client/src/pages/ViewCoursePageInfo/ReportAbuse/ReportAbuse.tsx
@@ -1,6 +1,6 @@
 const ReportAbuse = () => {
   return (
-    <div className="border border-black rounded-[0.2em] p-[0.8em] text-center hover:bg-hoverDivGray cursor-pointer">
+    <div className="border border-black rounded-[0.2em] p-[0.8em] text-center hover:bg-hoverDivGray cursor-pointer w-[650px]">
       <button>Report abuse</button>
     </div>
   );
