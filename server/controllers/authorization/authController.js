99ac68ba4930dbbbadda43037c20baa878946388
yosefhayim <yosefisabag+03@gmail.com>
2025-01-13 01:08:10 +0200

    j

--- server/controllers/authorization/authController.js
+++ server/controllers/authorization/authController.js
@@ -14,7 +14,6 @@ const generateToken = (payload) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-  console.log(token);
   return token;
 };
 
