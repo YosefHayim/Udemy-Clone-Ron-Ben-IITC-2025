997945057443a9b550ab377df0170f4269ffdcd2
yosefhayim <yosefisabag+03@gmail.com>
2025-01-12 23:48:04 +0200

    ds

--- server/utils/cookieOptions.js
+++ server/utils/cookieOptions.js
@@ -2,7 +2,7 @@ const cookieOptions = {
   httpOnly: false, // allow client-side JavaScript from accessing the cookie
   secure: process.env.NODE_ENV === "production", // Send cookie only over HTTPS in production
   sameSite: "None", // Required for cross-origin cookies
-  maxAge: process.env.JWT_EXPIRES_IN, // Expiry time in milliseconds
+  maxAge: parseInt(process.env.JWT_EXPIRES_IN, 10), // Expiry time in milliseconds
 };
 
 module.exports = cookieOptions;
