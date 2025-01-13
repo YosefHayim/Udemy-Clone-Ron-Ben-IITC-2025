13aef42af8d57464f5ebe739e0e92b331b07dbba
yosefhayim <yosefisabag+03@gmail.com>
2025-01-13 14:12:43 +0200

    asd

--- server/routes/course/courseRoute.js
+++ server/routes/course/courseRoute.js
@@ -24,7 +24,10 @@ router.get("/", getAllCourses);
 
 // Get course by specific course id
 router.get("/:id", getCourseById);
-router.get("/:courseId", getCourseProsById);
+router.get("/pros/:courseId", getCourseProsById);
+
+// Re-activate course by the authorized creator
+router.post("/re-activate/:id", grantedAccess, reactivateCourseById);
 
 // Create course
 router.post("/", grantedAccess, createCourse);
@@ -34,8 +37,5 @@ router.put("/:id", grantedAccess, updateCourse);
 
 // Delete course by course id
 router.delete("/:id", grantedAccess, deleteCourse);
-
-// Re-activate course by the authorized creator
-router.get("/re-activate/:id", grantedAccess, reactivateCourseById);
 
 module.exports = router;
