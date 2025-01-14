443179f6b770875cb68b1a67d9b954008385aaa5
yosefhayim <yosefisabag+03@gmail.com>
2025-01-14 14:00:01 +0200

    dsa

--- client/src/pages/Search/CourseHoverCardInfo/CourseHoverCardInfo.tsx
+++ client/src/pages/Search/CourseHoverCardInfo/CourseHoverCardInfo.tsx
@@ -4,8 +4,11 @@ import CoursePros from "./CoursePros/CoursePros";
 
 const CourseHoverCardInfo = ({ whatYouWillLearn, courseId, coursePrice }) => {
   return (
-    <div className="w-full flex flex-col items-center justify-center">
-      <div>
+    <div
+      className="w-full flex flex-col items-center justify-center"
+      id={courseId}
+    >
+      <div id={courseId} className={courseId}>
         <TriangleShape />
         <div className="w-[400px] rounded-[0.5em] bg-white p-[1.5em] shadow-2xl">
           <CoursePros whatYouWillLearn={whatYouWillLearn} />
