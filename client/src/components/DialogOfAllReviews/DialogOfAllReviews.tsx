3614b7bcc7fc92c6814d2f08eb4b06634bc3500d
yosefhayim <yosefisabag+03@gmail.com>
2025-01-14 19:52:18 +0200

    dsa

--- client/src/components/DialogOfAllReviews/DialogOfAllReviews.tsx
+++ client/src/components/DialogOfAllReviews/DialogOfAllReviews.tsx
@@ -1,3 +1,4 @@
+import getAllReviewsByCourseId from "@/api/reviews/getAllReviewsByCourseId";
 import {
   Dialog,
   DialogContent,
@@ -5,39 +6,128 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { useQuery } from "@tanstack/react-query";
 import { LuDot } from "react-icons/lu";
+import { useParams } from "react-router-dom";
+import Loader from "../Loader/Loader";
+import UserCourseReview from "@/pages/ViewCoursePageInfo/ReviewsSection/UserCourseReview/UserCourseReview";
+import { Button } from "../ui/button";
+import { MdOutlineStarPurple500, MdSearch } from "react-icons/md";
+import { Input } from "../ui/input";
 
 const DialogOfAllReviews = ({ avgRating, isClicked, setClicked }) => {
+  const params = useParams();
+  const courseId = params.courseId;
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["reviews", courseId],
+    queryFn: () => getAllReviewsByCourseId(courseId),
+    enabled: isClicked, // Prevent fetching until dialog is opened
+  });
+
   return (
     <Dialog open={isClicked} onOpenChange={setClicked}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>
             <div className="flex flex-row items-center justify-start">
-              {avgRating} course rating <LuDot />
-              3K ratings
+              {avgRating} course rating <LuDot /> 3K ratings
             </div>
           </DialogTitle>
-
           <DialogDescription>
-            <div className="flex flex-row items-center justify-center">
-              <div className="bg-red-400 w-1/3">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-                  maxime cumque aut nam debitis! Reprehenderit commodi, ducimus
-                  fugit harum magni quidem, optio cumque labore possimus
-                  perferendis
-                </p>
+            {isLoading && <Loader />}
+            {error && (
+              <div className="text-red-500">
+                Error loading reviews. Please try again later.
               </div>
-              <div className="bg-blue-400">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-                  maxime cumque aut nam debitis! Reprehenderit commodi, ducimus
-                  fugit harum magni quidem, optio cumque labore possimus
-                  perferendis
-                </p>
+            )}
+            {data && (
+              <div className="flex flex-row items-start justify-between w-full">
+                <div>
+                  <div className="flex flex-col items-start justify-start gap-[1em] my-[2em]">
+                    <div className="flex flex-row items-center justify-start">
+                      <progress value={20} max={100}></progress>
+                      {Array(5)
+                        .fill(null)
+                        .map((_, index) => (
+                          <MdOutlineStarPurple500
+                            key={index}
+                            className="text-[#f69c08]"
+                          />
+                        ))}
+                      <p className="underline text-[#6d28d2]">1%</p>
+                    </div>
+                    <div className="flex flex-row items-center justify-start gap-[0.1em]">
+                      <progress value={20} max={100}></progress>
+                      {Array(4)
+                        .fill(null)
+                        .map((_, index) => (
+                          <MdOutlineStarPurple500
+                            key={index}
+                            className="text-[#f69c08]"
+                          />
+                        ))}
+                      <p className="underline text-[#6d28d2]">1%</p>
+                    </div>
+                    <div className="flex flex-row items-center justify-start">
+                      <progress value={20} max={100}></progress>
+                      {Array(3)
+                        .fill(null)
+                        .map((_, index) => (
+                          <MdOutlineStarPurple500
+                            key={index}
+                            className="text-[#f69c08]"
+                          />
+                        ))}
+                      <p className="underline text-[#6d28d2]">1%</p>
+                    </div>
+                    <div className="flex flex-row items-center justify-start">
+                      <progress value={20} max={100}></progress>
+                      {Array(2)
+                        .fill(null)
+                        .map((_, index) => (
+                          <MdOutlineStarPurple500
+                            key={index}
+                            className="text-[#f69c08]"
+                          />
+                        ))}
+                      <p className="underline text-[#6d28d2]">1%</p>
+                    </div>
+                    <div className="flex flex-row items-center justify-start">
+                      <progress value={20} max={100}></progress>
+                      {Array(1)
+                        .fill(null)
+                        .map((_, index) => (
+                          <MdOutlineStarPurple500
+                            key={index}
+                            className="text-[#f69c08]"
+                          />
+                        ))}
+                      <p className="underline text-[#6d28d2]">1%</p>
+                    </div>
+                  </div>
+                  <form className="flex flex-row items-center justify-center gap-[0.5em]">
+                    <Input
+                      placeholder="Search reviews"
+                      type="text"
+                      className="border border-black focus:outline-none"
+                    />
+                    <Button className="border border-black hover:bg-[#1739531f] py-[0.5em] px-[0.7em] bg-white">
+                      <MdSearch className="text-black" />
+                    </Button>
+                  </form>
+                </div>
+                <div className="overflow-y-auto h-[600px] w-2/3">
+                  {data.map((review) => (
+                    <UserCourseReview
+                      review={review}
+                      key={review._id}
+                      widthOfReview={`w-full`}
+                    />
+                  ))}
+                </div>
               </div>
-            </div>
+            )}
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
