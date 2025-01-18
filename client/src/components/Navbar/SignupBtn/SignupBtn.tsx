ec9910ce137ec0ebbd7afd9b155b8f4182a6fa8d
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-18 17:51:39 +0200

    signup

--- client/src/components/Navbar/SignupBtn/SignupBtn.tsx
+++ client/src/components/Navbar/SignupBtn/SignupBtn.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SignupBtn = () => {
   return (
     <Link to="/signup">
-      <Button className="text-white bg-black font-bold px-4 py-2 rounded hover:opacity-70 transition duration-150 text-sm font-Sans">
+      <Button className="font-bold hover:opacity-70 transition duration-150 text-sm font-Sans bg-purple-700 text-white rounded-md px-4 py-2 hover:bg-purple-800 focus:outline-none">
         Sign up
       </Button>
     </Link>
