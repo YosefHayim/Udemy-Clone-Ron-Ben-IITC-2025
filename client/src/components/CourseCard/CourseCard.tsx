db46ce70f27eb2f23b24289ddcca7dcc9f4824db
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-19 23:59:03 +0200

    carousela

--- client/src/components/CourseCard/CourseCard.tsx
+++ client/src/components/CourseCard/CourseCard.tsx
@@ -1,31 +1,31 @@
-import { CourseCardProps } from "@/types/types";
+// import { CourseCardProps } from "@/types/types";
 
-const CourseCard: React.FC<CourseCardProps> = ({
-  title,
-  image,
-  description,
-  fullPrice,
-  discountPrice,
-  tag,
-}) => {
-  return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden">
-      <img src={image} alt={title} className="w-full h-32 object-cover" />
-      <div className="p-4">
-        <h3 className="font-bold text-lg">{title}</h3>
-        <p className="text-gray-600 text-sm">{description}</p>
-        <div className="mt-2">
-          <span className="text-red-500 font-bold">${discountPrice}</span>
-          <span className="text-gray-500 line-through ml-2">${fullPrice}</span>
-        </div>
-        {tag && (
-          <span className="text-xs text-white bg-yellow-500 px-2 py-1 rounded mt-2 inline-block">
-            {tag}
-          </span>
-        )}
-      </div>
-    </div>
-  );
-};
+// const CourseCard: React.FC<CourseCardProps> = ({
+//   title,
+//   image,
+//   description,
+//   fullPrice,
+//   discountPrice,
+//   tag,
+// }) => {
+//   return (
+//     <div className="bg-white shadow-md rounded-lg overflow-hidden">
+//       <img src={image} alt={title} className="w-full h-32 object-cover" />
+//       <div className="p-4">
+//         <h3 className="font-bold text-lg">{title}</h3>
+//         <p className="text-gray-600 text-sm">{description}</p>
+//         <div className="mt-2">
+//           <span className="text-red-500 font-bold">${discountPrice}</span>
+//           <span className="text-gray-500 line-through ml-2">${fullPrice}</span>
+//         </div>
+//         {tag && (
+//           <span className="text-xs text-white bg-yellow-500 px-2 py-1 rounded mt-2 inline-block">
+//             {tag}
+//           </span>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default CourseCard;
+// export default CourseCard;
