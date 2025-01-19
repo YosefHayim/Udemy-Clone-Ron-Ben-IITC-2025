215d3e7005664c4696720f32fdee67b01e44a370
yosefhayim <yosefisabag+03@gmail.com>
2025-01-20 01:26:52 +0200

    dsa

--- client/src/components/AtagBtn/AtagBtn.tsx
+++ client/src/components/AtagBtn/AtagBtn.tsx
@@ -1,18 +1,3 @@
-// import { AtagBtnProps } from "@/types/types";
-
-// const AtagBtn: React.FC<AtagBtnProps> = ({ aTagName }) => {
-//   return (
-//     <a
-//       href="#"
-//       className="text-[#020202] font-normal text-sm font-Sans rounded-md hover:bg-purple-100 hover:text-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-300"
-//     >
-//       {aTagName}
-//     </a>
-//   );
-// };
-
-// export default AtagBtn;
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
--- client/src/pages/Subscription/Subscription.tsx
+++ client/src/pages/Subscription/Subscription.tsx
@@ -1,26 +1,64 @@
+import { Button } from "@/components/ui/button";
+import { IoMdCheckmark } from "react-icons/io";
+import subscriptionImg from "/images/subscription-img.png";
+
 const Subscription = () => {
   return (
     <div className="p-[5em]">
       <h1 className="font-bold mb-[1em]">Subscriptions</h1>
-      <div className="flex flex-row items-start justify-start gap-[1em]">
-        <h3>Manage your Udemy subscriptions</h3>
-        <h3>Active plans</h3>
+      <div className="flex flex-col items-start justify-start gap-[1em]">
+        <h2 className=" text-[1.5em]">Manage your Udemy subscriptions</h2>
+        <h3 className="font-bold text-[1.5em] mb-[0.5em]">Active plans</h3>
       </div>
-      <hr />
-      <div>
-        <div className="flex flex-row items-center justify-center gap-[10em] mt-[1.5em]">
-          <div className="flex flex-col items-start justify-normal">
-            <h3 className="font-bold">Date</h3>
-          </div>
-          <div>
-            <h3 className="font-bold">Total price</h3>
-          </div>
-          <div>
-            <h3 className="font-bold">Payment type </h3>
+      <div className="h-[10em] flex flex-row items-center justify-center p-[2em] border-dashed border-2 border-gray-200 w-full mb-[2em]">
+        <div>
+          <p>You don’t have any active subscriptions</p>
+        </div>
+      </div>
+      {/* Here we render the data of history purchases of courses etc */}
+      <h3 className="font-bold text-[1.5em] mb-[0.5em]">
+        Subscription plans available
+      </h3>
+      <div className="flex flex-row items-center justify-between border border-gray-200">
+        <div className=" p-[2em]">
+          <div className="flex flex-row items-center justify-between">
+            <div>
+              <h4 className="mb-[1.5em] font-bold">Personal Plan</h4>
+              <ul className="flex flex-col items-start justify-start gap-[0.5em] mb-[1em]">
+                <p>
+                  New opportunities await. Sign up for Personal Plan to get all
+                  this and more:
+                </p>
+                <li className="flex flex-row items-center justify-start gap-[0.5em]">
+                  <IoMdCheckmark />
+                  <p>Access to 12,000+ top courses</p>
+                </li>
+                <li className="flex flex-row items-center justify-start gap-[0.5em]">
+                  <IoMdCheckmark />
+                  <p>Courses in tech, business, and more</p>
+                </li>
+                <li className="flex flex-row items-center justify-start gap-[0.5em]">
+                  <IoMdCheckmark />
+                  <p>Practice tests, exercises, and Q&A</p>
+                </li>
+              </ul>
+              <div className="flex flex-col items-start justify-start gap-[1em]">
+                <div className="flex flex-row items-start justify-start gap-[1em]">
+                  <Button className="text-white font-bold bg-[#6d28d2] rounded-[0.2em] hover:bg-[#892de1] h-[3em]">
+                    Subscribe
+                  </Button>
+                  <Button className="font-bold text-[#6d28d2] bg-white hover:bg-white shadow-none hover:hover-color-mix rounded-[0.2em] h-[3em]">
+                    Learn more
+                  </Button>
+                </div>
+                <p>Starting at ₪66.67 per month. Cancel anytime.</p>
+              </div>
+            </div>
           </div>
         </div>
-        <hr className="mt-[1em]" />
-        {/* Here we render the data of history purchases of courses etc */}
+        <div>
+          <img src={subscriptionImg} alt="" className="h-[25em]" />
+        </div>
       </div>
     </div>
   );
