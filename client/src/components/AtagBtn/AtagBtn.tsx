45c765df556e7c095d9dcaab1e195d7e96c7905b
ron959 <9592414@gmail.com>
2025-01-20 12:01:25 +0200

    chenaged link insted # to .

--- client/src/components/AtagBtn/AtagBtn.tsx
+++ client/src/components/AtagBtn/AtagBtn.tsx
@@ -13,7 +13,7 @@ const AtagBtn: React.FC<{ aTagName: string }> = ({ aTagName }) => {
       {/* Botão Principal */}
 
       <Link
-        to="#"
+        to="."
         className="text-[#020202] font-normal text-sm font-Sans rounded-md px-3
         py-4 hover:bg-purple-100 hover:text-purple-700 focus:outline-none
         focus:ring-2 focus:ring-purple-300"
