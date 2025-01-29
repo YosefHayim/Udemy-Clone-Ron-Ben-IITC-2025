675795380da5b97a6815560ddb6dc9521890ffee
Yosef Hayim <yosefisabag+03@gmail.com>
2025-01-29 23:05:17 +0200

    fds

---
+++ client/src/api/users/buyCourseByCourseId.ts
@@ -0,0 +1,32 @@
+import { axiosClient, localhostUrl } from "../configuration";
+
+type fn = (courseId: string) => Promise<any>;
+
+const byCourseByCourseId: fn = async (courseId: string) => {
+  if (!courseId || typeof courseId !== "string") {
+    console.error("Invalid course ID provided.");
+    return null;
+  }
+
+  const sanitizedCourseId = courseId.trim();
+  console.log(sanitizedCourseId);
+  const url = `${localhostUrl}/api/user/add/course/${sanitizedCourseId}`;
+
+  try {
+    const response = await axiosClient.post(url);
+
+    if (response?.data?.data) {
+      console.log(response);
+
+      return response.data.data;
+    }
+
+    console.warn("No course data found in the response.");
+    return null;
+  } catch (error) {
+    console.error(`Error fetching course with ID ${sanitizedCourseId}:`, error);
+    throw error;
+  }
+};
+
+export default byCourseByCourseId;
--- server/seedDb.js
+++ server/seedDb.js
@@ -113,8 +113,10 @@ const createCourses = async () => {
       }),
       courseRecapInfo: faker.lorem.words(10),
       courseDescription: faker.lorem.paragraph(),
-      courseFullPrice: faker.commerce.price(500, 1000),
-      courseDiscountPrice: faker.commerce.price(50, 300),
+      // courseFullPrice: faker.commerce.price(500, 1000),
+      // courseDiscountPrice: faker.commerce.price(50, 300),
+      courseFullPrice: 0,
+      courseDiscountPrice: 0,
       whoThisCourseIsFor: faker.lorem.sentence(),
       courseInstructorDescription: faker.lorem.paragraphs(10),
       whatYouWillLearn: Array.from({ length: 8 }, () => faker.lorem.sentence()),
@@ -602,17 +604,17 @@ const generateUpdatedDummyData = async () => {
     // await clearCollections();
     // console.log("Deleted all db.");
 
-    // console.log("Seeding users...");
-    // const users = await createUsers();
-    // console.log(`${users.length} users created.`);
+    console.log("Seeding users...");
+    const users = await createUsers();
+    console.log(`${users.length} users created.`);
 
-    // console.log("Seeding courses...");
-    // const courses = await createCourses();
-    // console.log(`${courses.length} courses created.`);
+    console.log("Seeding courses...");
+    const courses = await createCourses();
+    console.log(`${courses.length} courses created.`);
 
-    // console.log("Seeding sections...");
-    // const sections = await createSections();
-    // console.log(`${sections.length} sections created.`);
+    console.log("Seeding sections...");
+    const sections = await createSections();
+    console.log(`${sections.length} sections created.`);
 
     console.log("Seeding lessons...");
     const lessons = await createLessons();
