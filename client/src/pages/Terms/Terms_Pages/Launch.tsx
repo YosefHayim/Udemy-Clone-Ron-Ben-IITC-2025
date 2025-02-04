140e84cc4a5dd9168415f015bf6b5cf8559299dd
Ben Kilinski <ben.kilinski@gmail.com>
2025-02-04 14:12:34 +0200

    asdfa

--- client/src/pages/Terms/Terms_Pages/Launch.tsx
+++ client/src/pages/Terms/Terms_Pages/Launch.tsx
@@ -1,7 +1,128 @@
 const Launch = () => {
-    return(
+    return (
         <>
-        <h1>Launch name</h1>
+
+            <div className="bg-white text-gray-800 p-8 space-y-8">
+                <h1 className="text-3xl font-bold">Launch Services</h1>
+                <p className="text-sm text-gray-600">This Agreement was last updated on June 1, 2023.</p>
+
+                <div className="space-y-6">
+                    <section>
+                        <h2 className="text-xl font-semibold">Scope of Launch Services</h2>
+                        <p className="text-base leading-relaxed">
+                            Udemy will provide access to a dedicated project manager and launch program to assist
+                            Customers with the launch of the Services as further detailed in Schedule A
+                            (“<strong>Launch Services</strong>”). The provision of the Launch Services is contingent
+                            upon the Customer purchasing subscriptions to use the Services which are provided
+                            pursuant to the Master Services Agreement (“<strong>MSA</strong>”). Except as
+                            specifically stated herein, the MSA shall continue in full force and effect and is
+                            supplemental to and incorporated herein by reference.
+                        </p>
+                    </section>
+
+                    <section>
+                        <h2 className="text-xl font-semibold">Customer Cooperation/Responsibilities</h2>
+                        <p className="text-base leading-relaxed">
+                            Customer will cooperate reasonably and in good faith with Udemy in performance of Launch
+                            Services by, without limitation:
+                        </p>
+                        <ul className="list-disc list-inside ml-6 space-y-2">
+                            <li>Allocating sufficient resources and timely performing any tasks reasonably necessary to enable Udemy to perform its obligations under each Order Form;</li>
+                            <li>Timely delivering any materials and other obligations required under each Order Form;</li>
+                            <li>Timely responding to Udemy inquiries related to the Launch Services;</li>
+                            <li>
+                                Assigning an internal project manager for each Order Form to serve as a primary point
+                                of contact for Udemy;
+                            </li>
+                            <li>
+                                Actively participating in scheduled project meetings, in a timely manner and at no
+                                charge to Udemy, access to the appropriate and knowledgeable employees and agents,
+                                continuous administrative access to Udemy, and coordination of remote online and
+                                telephonic meetings all as reasonably required by Udemy; and
+                            </li>
+                            <li>
+                                Complete, accurate and timely information, data and feedback, all as reasonably
+                                required.
+                            </li>
+                        </ul>
+                    </section>
+
+                    <section>
+                        <h2 className="text-xl font-semibold">Term</h2>
+                        <p className="text-base leading-relaxed">
+                            This Addendum will remain in effect until the earlier of (a) completion of the Launch
+                            Services or (b) 9 months from the date of the Order Form.
+                        </p>
+                    </section>
+
+                    <section>
+                        <h2 className="text-xl font-semibold">Fees and Expenses</h2>
+                        <p className="text-base leading-relaxed">
+                            All Launch Services are billed and invoiced in accordance with the terms of the MSA upon
+                            receipt of a signed Order Form. All amounts for Launch Services, including Launch
+                            Service related travel and expenses, are due and payable in line with the terms of the
+                            Order Form.
+                        </p>
+                    </section>
+                </div>
+            </div>
+
+            <div className="max-w-3xl mx-auto">
+                <div className="mb-8">
+                    <h2 className="text-xl font-semibold mb-2">Independent Contractor</h2>
+                    <p className="text-base leading-relaxed">
+                        Each Party, including employees and consultants, will at all times maintain the status of independent contractor with respect to one another.
+                    </p>
+                </div>
+
+                <div className="mb-8">
+                    <h2 className="text-xl font-semibold mb-2">Intellectual Property</h2>
+                    <p className="text-base leading-relaxed">
+                        Except as expressly set forth in this Addendum, neither party grants the other party, express or implied, Intellectual Property Rights.
+                    </p>
+                </div>
+
+                <div className="mb-8">
+                    <h2 className="text-xl font-semibold mb-2">Warranties and Disclaimer</h2>
+                    <p className="text-base leading-relaxed">
+                        Udemy warrants that the Launch Services will be performed in a professional and workmanlike manner in accordance with generally accepted industry standards, in conformance with Schedule A and Udemy will comply with all applicable laws in providing the Launch Services...
+                    </p>
+                </div>
+
+                <div className="mb-8">
+                    <h2 className="text-xl font-semibold mb-2">Schedule A</h2>
+                    <p className="text-base leading-relaxed">
+                        Launch Services assist customers with the launch of Udemy products by providing a Project Manager to accelerate time to launch. The Udemy team provides Launch Services that may include the following phases:
+                    </p>
+                    <ul className="list-disc pl-6 mt-2">
+                        <li><strong>Discover:</strong> The discovery phase is an opportunity to connect Udemy and customer stakeholders.</li>
+                        <li><strong>Plan:</strong> All project plan development including definition, sequencing, timeline, etc.</li>
+                        <li><strong>Execute:</strong> All product configuration is completed in the Execute phase.</li>
+                        <li><strong>Launch:</strong> The Launch phase begins when all elements of the execution phase are completed.</li>
+                        <li><strong>Post-Launch:</strong> The Project Manager will support the project for 10 business days post go-live.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="max-w-3xl mx-auto">
+
+                <div className="mb-8">
+                    <h2 className="text-xl font-semibold mb-2">Schedule A</h2>
+                    <p className="text-base leading-relaxed">
+                        Launch Services assist customers with the launch of Udemy products by providing a Project Manager to accelerate time to launch. The Udemy team provides Launch Services that may include the following phases:
+                    </p>
+                    <ul className="list-disc pl-6 mt-2">
+                        <li><strong>Discover:</strong> The discovery phase is an opportunity to connect Udemy and customer stakeholders. The discovery phase includes a project kick-off meeting including; contract review, goal alignment, and presentation of Launch Services. After the project kick-off is completed, mobilization activities commence including; stakeholder alignment, proposed launch timeline, scoping, and resource coordination.</li>
+                        <li><strong>Plan:</strong> All project plan development including definition, sequencing, timeline, duration, milestone development and resource planning is created during the Plan phase. Any risks or customer dependencies are identified and communicated to customer stakeholders.</li>
+                        <li><strong>Execute:</strong> All product configuration is completed in the Execute phase. The Project Manager provides weekly project status reviews communicated via email. The Project Manager will orchestrate collaboration with Udemy subject matter experts to provide the customer with resources to support fulfillment of each milestone. Udemy product configuration, license allocation, resource coordination, and change management planning are completed during this phase to prepare for launch.</li>
+                        <li><strong>Launch:</strong> The Launch phase begins when all elements of the execution phase are completed. The Launch phase includes customer training, communication plan and templates provided, and product(s) launch.</li>
+                        <li><strong>Post-Launch:</strong> The Project Manager will support the project for 10 business days post go-live and during this time will monitor launch to check goal progress, identify areas for improvement, and optimize success metrics. The Project Manager will provide a formal email communication to conclude the launch project.</li>
+                    </ul>
+                </div>
+            </div>
+
+
+
         </>
     )
 }
