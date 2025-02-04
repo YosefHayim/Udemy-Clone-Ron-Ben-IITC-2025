de6a51e6476df961c0e255404833701c0a4cc0b7
YosefHayim <yosefisabag+03@gmail.com>
2025-02-04 09:46:34 +0200

    fd

--- client/src/pages/Terms/Side_bar_terms.tsx
+++ client/src/pages/Terms/Side_bar_terms.tsx
@@ -1,41 +1,41 @@
 const Side_bar_terms = ({ setSelectedPage, selectedPage }) => {
-    const menuItems = [
-        "Terms of Use",
-        "Privacy policy",
-        "Intellectual Property Policy",
-        "Udemy API Agreement",
-        "Master Services Agreement",
-        "Udemy Business Privacy Statement",
-        "Instructor Terms",
-        "Affiliate Terms & Conditions",
-        "Udemy Business Leadership Academy Terms & Conditions",
-        "Udemy Business Pro Terms & Conditions",
-        "Launch Services",
-        "Pricing and Promotions Policy",
-    ];
+  const menuItems = [
+    "Terms of Use",
+    "Privacy Policy",
+    "Intellectual Property Policy",
+    "Udemy API Agreement",
+    "Master Services Agreement",
+    "Udemy Business Privacy Statement",
+    "Instructor Terms",
+    "Affiliate Terms & Conditions",
+    "Udemy Business Leadership Academy Terms & Conditions",
+    "Udemy Business Pro Terms & Conditions",
+    "Launch Services",
+    "Pricing and Promotions Policy",
+  ];
 
-    return (
-        <div className="h-screen w-[16rem] bg-white shadow-md">
-            {/* Menu lateral */}
-            <aside className="w-64">
-                <ul className="py-4">
-                    {menuItems.map((item, index) => (
-                        <li
-                            key={index}
-                            className={`px-6 py-2 cursor-pointer text-base font-[790] ${
-                                selectedPage === item
-                                    ? "bg-black text-white opacity-86"
-                                    : "text-[#303141] text-opacity-80 hover:text-black"
-                            }`}
-                            onClick={() => setSelectedPage(item)} // Atualiza a página ativa
-                        >
-                            {item}
-                        </li>
-                    ))}
-                </ul>
-            </aside>
-        </div>
-    );
+  return (
+    <div className="h-screen w-[16rem] bg-white shadow-md">
+      {/* Menu lateral */}
+      <aside className="w-64">
+        <ul className="py-4">
+          {menuItems.map((item, index) => (
+            <li
+              key={index}
+              className={`px-6 py-2 cursor-pointer text-base font-[790] ${
+                selectedPage === item
+                  ? "bg-black text-white opacity-86"
+                  : "text-[#303141] text-opacity-80 hover:text-black"
+              }`}
+              onClick={() => setSelectedPage(item)} // Atualiza a página ativa
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </div>
+  );
 };
 
 export default Side_bar_terms;
--- client/src/pages/Terms/Terms.tsx
+++ client/src/pages/Terms/Terms.tsx
@@ -15,59 +15,62 @@ import Master from "./Terms_Pages/Master";
 
 // Adicione os outros componentes conforme necessário
 const Terms = () => {
-    const [selectedPage, setSelectedPage] = useState("Terms of Use"); // Estado inicial
+  const [selectedPage, setSelectedPage] = useState("Terms of Use"); // Estado inicial
 
-    // Função que retorna o componente correspondente
-    const renderComponent = () => {
-        switch (selectedPage) {
-            case "Terms of Use":
-                return <Terms_of_use />;
-            case "Privacy Policy":
-                return <Privacy_policy2 />;
-            case "Intellectual Property Policy":
-                return <Intellectual />;
-            case "Udemy API Agreement":
-                return <Api_agreement />;
-            case "Master Services Agreement":
-                return <Master />;
-            case "Udemy Business Privacy Statement":
-                return <Business_statment />;
-            case "Instructor Terms":
-                return <Instructor />;
-            case "Affiliate Terms & Conditions":
-                return <Affiliate />;
-            case "Udemy Business Leadership Academy Terms & Conditions":
-                return <Leadership />;
-            case "Udemy Business Pro Terms & Conditions":
-                return <Pro_terms />;
-            case "Launch Services":
-                return <Launch />;
-            case "Pricing and Promotions Policy":
-                return <Promotions />;
-            default:
-                return <Terms_of_use />;
-        }
-    };
+  // Função que retorna o componente correspondente
+  const renderComponent = () => {
+    switch (selectedPage) {
+      case "Terms of Use":
+        return <Terms_of_use />;
+      case "Privacy Policy":
+        return <Privacy_policy2 />;
+      case "Intellectual Property Policy":
+        return <Intellectual />;
+      case "Udemy API Agreement":
+        return <Api_agreement />;
+      case "Master Services Agreement":
+        return <Master />;
+      case "Udemy Business Privacy Statement":
+        return <Business_statment />;
+      case "Instructor Terms":
+        return <Instructor />;
+      case "Affiliate Terms & Conditions":
+        return <Affiliate />;
+      case "Udemy Business Leadership Academy Terms & Conditions":
+        return <Leadership />;
+      case "Udemy Business Pro Terms & Conditions":
+        return <Pro_terms />;
+      case "Launch Services":
+        return <Launch />;
+      case "Pricing and Promotions Policy":
+        return <Promotions />;
+      default:
+        return <Terms_of_use />;
+    }
+  };
 
-    return (
-        <div className="flex h-screen">
-            {/* Sidebar */}
-            <div className="w-64">
-                <Side_bar_terms setSelectedPage={setSelectedPage} selectedPage={selectedPage} />
-            </div>
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="w-64">
+        <Side_bar_terms
+          setSelectedPage={setSelectedPage}
+          selectedPage={selectedPage}
+        />
+      </div>
 
-            {/* Área de Conteúdo */}
-            <div
-                className="flex-1 h-screen overflow-y-scroll custom-scrollbar px-[12rem]"
-                style={{
-                    scrollbarWidth: "none", // Firefox
-                    msOverflowStyle: "none", // IE e Edge
-                }}
-            >
-                {renderComponent()}
-            </div>
-        </div>
-    );
+      {/* Área de Conteúdo */}
+      <div
+        className="flex-1 h-screen overflow-y-scroll custom-scrollbar px-[12rem]"
+        style={{
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // IE e Edge
+        }}
+      >
+        {renderComponent()}
+      </div>
+    </div>
+  );
 };
 
 export default Terms;
