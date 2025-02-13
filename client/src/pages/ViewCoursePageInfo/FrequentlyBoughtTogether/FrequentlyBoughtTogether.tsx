12e8622a3bd915bc2fb05769359e099924365675
YosefHayim <yosefisabag+03@gmail.com>
2025-02-13 09:06:27 +0200

    fd

--- client/src/pages/ViewCoursePageInfo/FrequentlyBoughtTogether/FrequentlyBoughtTogether.tsx
+++ client/src/pages/ViewCoursePageInfo/FrequentlyBoughtTogether/FrequentlyBoughtTogether.tsx
@@ -1,11 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import FrequentlyCourseCard from "./FaqCourseCard/FaqCourseCard";
 import FaqTotalCoursesPrice from "./FaqTotalCoursesPrice/FaqTotalCoursesPrice";
+import getThreeCoursesOfInstructor from "@/api/courses/getThreeCoursesOfInstructor";
 
-const FrequentlyBoughtTogether: React.FC = () => {
+const FrequentlyBoughtTogether: React.FC<{ instructorId: string }> = ({
+  instructorId,
+}) => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["instructorThreeCourse"],
+    queryFn: () => getThreeCoursesOfInstructor(instructorId),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
     <div>
       <div className="flex flex-col border border-[#d1d7dc] w-[515px] p-[1em]">
         <h2 className="font-bold text-[1.5em]">Frequently Bought Together</h2>
+        {data.map((course) => console.log(course))}
         <FrequentlyCourseCard />
         <FrequentlyCourseCard />
         <FrequentlyCourseCard />
