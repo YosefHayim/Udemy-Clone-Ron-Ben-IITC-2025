7bda6e1d349f2b4d1ffb0eb5ab30dd6945285f63
yosefhayim <yosefisabag+03@gmail.com>
2025-03-10 20:02:48 +0200

    Update CourseRatings default values to zero for ratings and total ratings

--- client/src/components/CourseCard/CourseRatings/CourseRatings.tsx
+++ client/src/components/CourseCard/CourseRatings/CourseRatings.tsx
@@ -1,9 +1,9 @@
 import { CourseRatingsProps } from "@/types/types";
 
 const CourseRatings: React.FC<CourseRatingsProps> = ({
-  avgRatings = 4.6,
+  avgRatings = 0,
   stars = "★★★★☆",
-  totalRatings = 222759,
+  totalRatings = 0,
 }) => {
   return (
     <div className="flex flex-row text-gray-500  items-center gap-[0.2em]">
