a40c38576a4410995198ab901b98694b17262c1d
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-15 07:23:21 +0200

    Refactor FilterNSort component to simplify filter reset functionality. Introduce defaultFilters and a utility function for comparing filter states, enhancing clarity and maintainability. Update clear filters logic to only display when filters are active.

--- client/src/pages/Search/SidebarFilter/FilterNSort/FilterNSort.tsx
+++ client/src/pages/Search/SidebarFilter/FilterNSort/FilterNSort.tsx
@@ -1,42 +1,50 @@
+import { useContext } from "react";
 import FilterBtn from "./FilterBtn/FilterBtn";
 import SortDropDown from "./SortDropDown/SortDropDown";
-import { useContext } from "react";
 import { filterContext } from "@/routes/AppRoutes";
+
+const defaultFilters = {
+  sortBy: "",
+  handsOnPractice: new Set(),
+  language: new Set(),
+  levels: new Set(),
+  price: "",
+  ratings: 0,
+  subtitles: new Set(),
+  topics: new Set(),
+  videosDurations: new Set(),
+  certificateOnly: false,
+};
+
+const convertToComparable = (filters: any) =>
+  JSON.stringify(
+    Object.fromEntries(
+      Object.entries(filters).map(([key, value]) => [
+        key,
+        value instanceof Set ? [...value] : value,
+      ])
+    )
+  );
 
 const FilterNSort = () => {
   const [filterData, setFilterData] = useContext(filterContext);
-  const handleClick = () => {
-    const resetState = {
-      sortBy: "",
-      handsOnPractice: new Set(),
-      language: new Set(),
-      levels: new Set(),
-      price: "",
-      ratings: 0,
-      subtitles: new Set(),
-      topics: new Set(),
-      videosDurations: new Set(),
-      certificateOnly: false,
-    };
-    setFilterData(resetState);
-    console.log("Filters reset to:", resetState);
-  };
+
+  const isFiltersDefault =
+    convertToComparable(filterData) === convertToComparable(defaultFilters);
 
   return (
     <div className="flex w-full items-center justify-between mb-[2.4em]">
       <div className="flex items-center gap-[0.5em]">
-        <div>
-          <FilterBtn />
-        </div>
-        <div>
-          <SortDropDown />
-        </div>
-        <span
-          className="text-purpleStatic hover:text-purpleHover font-bold cursor-pointer"
-          onClick={handleClick}
-        >
-          Clear filters
-        </span>
+        <FilterBtn />
+        <SortDropDown />
+        {!isFiltersDefault && (
+          <span
+            className="text-purpleStatic hover:text-purpleHover font-bold cursor-pointer"
+            onClick={() => setFilterData(defaultFilters)}
+          >
+            Clear filters
+          </span>
+        )}
       </div>
     </div>
   );
