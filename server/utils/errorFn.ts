bb03c7690ec802744a265d64d0082f724d6c8001
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-22 15:54:26 +0200

    Refactor error handling middleware and utility to improve error response structure

--- server/utils/errorFn.ts
+++ server/utils/errorFn.ts
@@ -1,7 +1,12 @@
-const createError = (message: string, statusCode: number) => {
+import { Response } from "express";
+
+const createError = (res: Response, message: string, statusCode: number) => {
   const error = new Error(message);
-  // error.status = statusCode;
-  return error;
+  res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: error.message,
+  });
 };
 
 export default createError;
