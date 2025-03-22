c51a4242fd0c6cda08976e0259520a301c507e43
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-22 16:30:56 +0200

    Refactor AtagBtn component to enhance hover functionality and improve button links

--- client/src/components/AtagBtn/AtagBtn.tsx
+++ client/src/components/AtagBtn/AtagBtn.tsx
@@ -1,39 +1,60 @@
 import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { btnStyleNHover } from "@/utils/stylesStorage";
+import { Link } from "react-router-dom";
 
 const AtagBtn: React.FC<{ aTagName: string }> = ({ aTagName }) => {
   const [isHovering, setIsHovering] = useState(false);
 
   return (
     <div
-      className="relative mx-[0.2em] inline-block text-[1.4em]"
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
+      className="w-min-max relative mx-[0.2em] inline-block text-[1.4em]"
     >
       <p
-        className={` ${btnStyleNHover} rounded-md px-3 py-3 font-sans text-sm font-normal
-        text-[#020202]`}
+        className={`${btnStyleNHover} rounded-md px-3 py-3 font-sans text-sm font-normal text-[#020202]`}
       >
         {aTagName}
       </p>
-
-      {isHovering && aTagName !== "Explore" && (
-        <div>
+      <div>
+        {isHovering && (
           <div className="absolute right-0 top-[3.1em] z-50 flex w-[290px] flex-col items-center justify-center gap-[0.5em] rounded-lg border border-gray-300 bg-white p-4 text-center shadow-alertAlgoInfo">
-            <p className="font-sans font-extrabold leading-tight text-gray-800">
-              {aTagName === "Udemy Business"
-                ? "Get your team access to over 27,000 top Udemy courses, anytime, anywhere."
-                : "Turn what you know into an opportunity and reach millions around the world."}
-            </p>
-            <Button className="rounded-[0.2rem] bg-btnColor px-14 py-[1.2rem] font-sans font-sans text-sm font-extrabold text-white transition duration-150 hover:bg-[#892DE1] focus:outline-none">
-              {aTagName === "Udemy Business"
-                ? "Try Udemy Business"
-                : "Learn more"}
-            </Button>
+            {aTagName === "Udemy Business" && (
+              <>
+                <p className="font-sans font-extrabold leading-tight text-gray-800">
+                  Get your team access to over 27,000 top Udemy courses,
+                  anytime, anywhere.
+                </p>
+                <Button className="rounded-[0.2rem] bg-btnColor px-14 py-[1.2rem] font-sans text-sm font-extrabold text-white transition duration-150 hover:bg-[#892DE1] focus:outline-none">
+                  <Link to="/not-found">Try Udemy Business</Link>
+                </Button>
+              </>
+            )}
+            {aTagName === "My learning" && (
+              <>
+                <p className="font-sans font-extrabold leading-tight text-gray-800">
+                  Start learning from over 250,000 courses today.
+                </p>
+                <Button className="w-full rounded-[0.3em] border border-purple-800 bg-white px-2 py-3 font-sans font-extrabold text-purple-800 hover:bg-purple-100 focus:outline-none">
+                  <Link to="/">Browse now</Link>
+                </Button>
+              </>
+            )}
+            {aTagName === "Teach on Udemy" && (
+              <>
+                <p className="font-sans font-extrabold leading-tight text-gray-800">
+                  Turn what you know into an opportunity and reach millions
+                  around the world.
+                </p>
+                <Button className="rounded-[0.2rem] bg-btnColor px-14 py-[1.2rem] font-sans text-sm font-extrabold text-white transition duration-150 hover:bg-[#892DE1] focus:outline-none">
+                  <Link to="/teaching/?ref=teach_header">Learn more</Link>
+                </Button>
+              </>
+            )}
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
