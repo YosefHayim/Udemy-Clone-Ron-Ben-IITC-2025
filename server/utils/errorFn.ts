8285675e5d0c39a9de269c5a944f4a7e21303a31
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-22 15:57:03 +0200

    Refactor error creation utility to remove response handling and return error object

--- server/utils/errorFn.ts
+++ server/utils/errorFn.ts
@@ -1,12 +1,6 @@
-import { Response } from "express";
-
-const createError = (res: Response, message: string, statusCode: number) => {
+const createError = (message: string, statusCode: number) => {
   const error = new Error(message);
-  res.status(statusCode).json({
-    success: false,
-    statusCode: statusCode,
-    message: error.message,
-  });
+  return error;
 };
 
 export default createError;
