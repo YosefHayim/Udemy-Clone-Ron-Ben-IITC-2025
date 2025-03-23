c7224712686e9d3f50c6542af765f153bcc9ad21
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-23 12:45:00 +0200

    Refactor LetsStartLearning component to adjust button text size for improved readability

--- client/src/pages/Home/LetsStartLearning/LetsStartLearning.tsx
+++ client/src/pages/Home/LetsStartLearning/LetsStartLearning.tsx
@@ -4,19 +4,21 @@ import ContinueLearningCourse from "./ContinueLearningCourse/ContinueLearningCou
 
 const LetsStartLearning = () => {
   return (
-    <div className="flex w-full flex-col items-stretch justify-start gap-3 pl-4">
+    <div className="flex w-full flex-col items-stretch justify-start gap-6 pl-4">
       <div className="flex w-full flex-row items-center justify-between">
         <h1 className="font-extrabold">Let's start learning</h1>
         <div>
           <button
-            className={`${btnStyleNHover} rounded-md px-3 py-3 font-sans text-xs font-bold text-purple-700 underline`}
+            className={`${btnStyleNHover} rounded-md px-3 py-3 font-sans text-base font-bold text-purple-700 underline`}
           >
             My learning
           </button>
         </div>
       </div>
       <ContinueLearningCourse />
-      <StartWeeklyStreak />
+      <div className="my-4">
+        <StartWeeklyStreak />
+      </div>
     </div>
   );
 };
