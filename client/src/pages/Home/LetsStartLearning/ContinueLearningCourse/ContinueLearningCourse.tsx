7c56a8e3cd0597331b266c54eb1103dc0acf1275
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-23 12:43:38 +0200

    Refactor ContinueLearningCourse component to enhance layout and add course details with placeholder image

--- client/src/pages/Home/LetsStartLearning/ContinueLearningCourse/ContinueLearningCourse.tsx
+++ client/src/pages/Home/LetsStartLearning/ContinueLearningCourse/ContinueLearningCourse.tsx
@@ -1,5 +1,42 @@
+import { BsDot } from "react-icons/bs";
+import placeholderCourseViewImg from "/images/placeholderCourseViewImg.png";
+import { FaCirclePlay } from "react-icons/fa6";
+
 const ContinueLearningCourse = () => {
-  return <div>dsadasdsadsadsa</div>;
+  // Need to add course lesson id to navigate on each map of this.
+  return (
+    <div className="max-w-[350px] cursor-pointer hover:bg-gray-100">
+      <div className="flex flex-row items-start justify-start border border-gray-300">
+        <div className="relative h-[10em] w-[150px]">
+          <img
+            src={placeholderCourseViewImg}
+            alt=""
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute top-[30%] translate-x-3/4">
+            <FaCirclePlay
+              size={40}
+              className="rounded-full bg-gray-600 text-white"
+            />
+          </div>
+        </div>
+        <div className="flex h-[10em] w-full flex-col items-start justify-between p-2">
+          <div className="flex w-full flex-col gap-2">
+            <b className="text-gray-600">
+              Web design for Web Developers: Build...
+            </b>
+            <b>8. Working with Images</b>
+          </div>
+          <div className="flex items-center justify-start">
+            <b>Lecture</b>
+            <BsDot />
+            <p>4m</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContinueLearningCourse;
