c567224556fd051d69ab5cebda796d17661ce60a
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-27 23:55:38 +0200

    Update README with task list and modify LetsStartLearning component for cleaner code; adjust seedDb to create more courses.

--- server/seedDb.ts
+++ server/seedDb.ts
@@ -782,15 +782,15 @@ const generateUpdatedDummyData = async () => {
   try {
     await connectDb();
     console.log("Database connection established.");
-    await clearCollections();
-    console.log("Deleted all db.");
+    // await clearCollections();
+    // console.log("Deleted all db.");
 
     console.log("Seeding users...");
     const users = await createUsers();
     console.log(`${users.length} users created.`);
 
     console.log("Seeding courses...");
-    const courses = await createCourses({ coursesPerTopic: 30 });
+    const courses = await createCourses({ coursesPerTopic: 1000 });
     if (courses && courses.length > 1) {
       console.log(`${courses.length} courses created.`);
     }
