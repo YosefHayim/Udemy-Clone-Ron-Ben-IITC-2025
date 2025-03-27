04b038755f2c537a6ed1c039585a6ee408a9a219
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-28 01:32:28 +0200

    Refactor setUserInformation function for improved readability and structure

--- client/src/utils/setUserInformation.ts
+++ client/src/utils/setUserInformation.ts
@@ -21,28 +21,30 @@ import { AppDispatch } from '@/redux/store';
 
 export const setUserInformation = (cookie: string, dispatch: AppDispatch) => {
   if (!cookie) console.log('Cookie is not provided: ', cookie);
+  
   if (cookie) console.log(`Cookie received is: `, cookie);
+  {
+    const decoded = jwtDecode<DecodedTokenProps>(cookie);
+    console.log('user information has been updated: ', decoded);
 
-  const decoded = jwtDecode<DecodedTokenProps>(cookie);
-  console.log('user information has been updated: ', decoded);
+    if (decoded.fullName) dispatch(setFullName(decoded.fullName));
+    if (decoded.headline) dispatch(setHeadline(decoded.headline));
+    if (decoded.language) dispatch(setLanguage(decoded.language));
+    if (decoded.userLinks) dispatch(setUserLinks(decoded.userLinks));
+    if (decoded.profilePic) dispatch(setProfilePic(decoded.profilePic));
+    if (decoded.email) dispatch(setEmailAddress(decoded.email));
+    if (decoded.bio) dispatch(setBio(decoded.bio));
+    if (decoded.role) dispatch(setRole(decoded.role));
+    if (decoded.coursesBought) dispatch(setCoursesBought(decoded.coursesBought));
+    if (decoded.udemyCredits) dispatch(setUdemyCredits(decoded.udemyCredits));
+    if (decoded.createdAt) dispatch(setCreatedAt(decoded.createdAt));
+    if (decoded.createdAt) dispatch(setUpdatedAt(decoded.updatedAt));
+    if (decoded.isAuthActivated) dispatch(setAuthActivate(decoded.isAuthActivated));
+    if (decoded.isLoggedPreviouslyWithGoogle) dispatch(setIsLoggedWithGoogle(true));
+    dispatch(setCookie(cookie));
 
-  if (decoded.fullName) dispatch(setFullName(decoded.fullName));
-  if (decoded.headline) dispatch(setHeadline(decoded.headline));
-  if (decoded.language) dispatch(setLanguage(decoded.language));
-  if (decoded.userLinks) dispatch(setUserLinks(decoded.userLinks));
-  if (decoded.profilePic) dispatch(setProfilePic(decoded.profilePic));
-  if (decoded.email) dispatch(setEmailAddress(decoded.email));
-  if (decoded.bio) dispatch(setBio(decoded.bio));
-  if (decoded.role) dispatch(setRole(decoded.role));
-  if (decoded.coursesBought) dispatch(setCoursesBought(decoded.coursesBought));
-  if (decoded.udemyCredits) dispatch(setUdemyCredits(decoded.udemyCredits));
-  if (decoded.createdAt) dispatch(setCreatedAt(decoded.createdAt));
-  if (decoded.createdAt) dispatch(setUpdatedAt(decoded.updatedAt));
-  if (decoded.isAuthActivated) dispatch(setAuthActivate(decoded.isAuthActivated));
-  if (decoded.isLoggedPreviouslyWithGoogle) dispatch(setIsLoggedWithGoogle(true));
-  dispatch(setCookie(cookie));
-
-  if (decoded.isLoggedPreviouslyWithGoogle !== undefined) {
-    dispatch(setIsLoggedWithGoogle(decoded.isLoggedPreviouslyWithGoogle));
+    if (decoded.isLoggedPreviouslyWithGoogle !== undefined) {
+      dispatch(setIsLoggedWithGoogle(decoded.isLoggedPreviouslyWithGoogle));
+    }
   }
 };
