b1ac8ea93542faba0d56e7c76921998f39c023ad
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-28 04:35:24 +0300

    Remove unnecessary min-height from SidebarFilter for cleaner layout

--- client/src/pages/Search/SidebarFilter/SidebarFilter.tsx
+++ client/src/pages/Search/SidebarFilter/SidebarFilter.tsx
@@ -11,7 +11,7 @@ import { SidebarFilterProps } from '@/types/types';
 
 const SidebarFilter: React.FC<SidebarFilterProps> = () => {
   return (
-    <div className="relative min-h-max w-full">
+    <div className="w-full">
       <ViewCertificatesOnly />
       <RatingsFilter />
       <LanguageFilter />
