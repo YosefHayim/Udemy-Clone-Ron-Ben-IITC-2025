53bea8ae46bc544a3f8860abc566db41bcfd4825
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-29 21:48:58 +0300

    added close icon

--- client/src/components/MobileNavbar/MobileSidebar/MobileSidebar.tsx
+++ client/src/components/MobileNavbar/MobileSidebar/MobileSidebar.tsx
@@ -10,13 +10,13 @@ import {
   thirdDividerList,
 } from '@/utils/mobileSidebarCategories';
 import { IoIosArrowForward } from 'react-icons/io';
-import ChangeLanguage from '@/components/Navbar/DropDownMenu/ChangeLanguage/ChangeLanguage';
 import { Link } from 'react-router-dom';
+import { IoClose, IoCloseOutline } from 'react-icons/io5';
 
 const style = {
   width: '100%',
-  margin: '1px',
-  overflow: 'unset',
+  margin: '2px',
+  overflow: 'hidden',
   borderRadius: '5px',
   flexDirection: 'row',
   justifyContent: 'space-between',
@@ -28,68 +28,79 @@ const style = {
 
 const MobileSidebar = ({ showSidebar, setShowSidebar }) => {
   const DrawerList = (
-    <Box sx={{ width: 250, fontSize: '14px' }} role="presentation">
-      <List>
-        {firstDividerList.map((word) => (
-          <ListItem key={word.text} disablePadding>
-            <Link to={word.to} className="w-full">
-              <ListItemButton
-                sx={{
-                  width: '100%',
-                  margin: '1px',
-                  color: '#6d28d2',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  alignItems: 'start',
-                  ':hover': {
-                    background: '#EDE5FA',
-                  },
-                }}
-              >
-                <div className="flex w-full items-center justify-between">
-                  <p>{word.text}</p>
-                </div>
+    <div>
+      <Box
+        sx={{ width: 250, fontSize: '14px', overflowX: 'hidden', overflowY: 'hidden' }}
+        role="presentation"
+      >
+        <List>
+          {firstDividerList.map((word) => (
+            <ListItem key={word.text} disablePadding>
+              <Link to={word.to} className="w-full">
+                <ListItemButton
+                  sx={{
+                    width: '100%',
+                    margin: '2px',
+                    color: '#6d28d2',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'start',
+                    ':hover': {
+                      background: '#EDE5FA',
+                    },
+                  }}
+                >
+                  <div className="flex w-full items-center justify-between">
+                    <p>{word.text}</p>
+                  </div>
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+        <Divider sx={{ border: '1px solid gray-100' }} />
+        <List>
+          <ListItemButton sx={style}>
+            Certification preparation <IoIosArrowForward />
+          </ListItemButton>
+        </List>
+        <Divider />
+        <List>
+          <List sx={{ width: '100%', marginLeft: '25px', fontWeight: 700, color: 'gray' }}>
+            Most popular
+          </List>
+          {secondDividerList.map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton sx={style}>
+                <p>{text}</p>
+                <IoIosArrowForward />
               </ListItemButton>
-            </Link>
-          </ListItem>
-        ))}
-      </List>
-      <Divider sx={{ border: '1px solid gray-100' }} />
-      <List>
-        <ListItemButton sx={style}>
-          Certification preparation <IoIosArrowForward />
-        </ListItemButton>
-      </List>
-      <Divider />
-      <List>
-        <List sx={{ width: '100%', marginLeft: '25px', fontWeight: 700, color: 'gray' }}>
-          Most popular
+            </ListItem>
+          ))}
         </List>
-        {secondDividerList.map((text) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton sx={style}>
-              <p>{text}</p>
-              <IoIosArrowForward />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        <List sx={{ width: '100%', marginLeft: '25px', fontWeight: 700, color: 'gray' }}>
-          More from Udemy
+        <Divider />
+        <List>
+          <List sx={{ width: '100%', marginLeft: '25px', fontWeight: 700, color: 'gray' }}>
+            More from Udemy
+          </List>
+          {thirdDividerList.map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton sx={style}>{text}</ListItemButton>
+            </ListItem>
+          ))}
         </List>
-        {thirdDividerList.map((text) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton sx={style}>{text}</ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+      </Box>
+      <div
+        className="fixed left-[260px] top-5 z-[1301] cursor-pointer rounded-full bg-white p-2 shadow-md hover:bg-gray-100"
+        onClick={() => setShowSidebar(false)}
+      >
+        <IoCloseOutline size={22} />
+      </div>
+    </div>
   );
 
   return (
-    <div className="text-black">
+    <div>
       <Drawer open={showSidebar} onClose={() => setShowSidebar(false)}>
         {DrawerList}
       </Drawer>
