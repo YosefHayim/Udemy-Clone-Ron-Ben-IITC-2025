4d0c4bf8d6cbe7ebe9ec0da2c14516bd9756f72e
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-29 15:51:59 +0300

    Refactor CheckboxSpecialOffer to use local Checkbox component and update required prop for special offers

--- client/src/pages/SignUp/SignUpForm/CheckboxSpecialOffer/CheckboxSpecialOffer.tsx
+++ client/src/pages/SignUp/SignUpForm/CheckboxSpecialOffer/CheckboxSpecialOffer.tsx
@@ -1,4 +1,4 @@
-import { Checkbox } from '@radix-ui/react-checkbox';
+import { Checkbox } from '@/components/ui/checkbox';
 
 const CheckboxSpecialOffer = () => {
   return (
@@ -6,8 +6,8 @@ const CheckboxSpecialOffer = () => {
       <div className="w-full flex-col flex-wrap items-center justify-center gap-1">
         <Checkbox
           id="offers"
-          required={false}
-          className="absolute mr-[0.5em] h-4 w-4 scale-90 rounded-[0.2em] border-2 border-black ring-0 focus:outline-none focus:ring-0"
+          required={true}
+          className="k absolute mr-[0.5em] h-4 w-4 scale-90 rounded-[0.2em] ring-0 focus:outline-none focus:ring-0"
         />
         <label htmlFor="offers" className="ml-5 text-sm">
           Send me special offers, personalized recommendations, and learning tips.
