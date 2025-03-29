c8c04986b9bcdadd8d38ff6387657698f934bf98
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-29 21:26:59 +0300

    working on sidebar

--- client/src/components/MobileNavbar/MobileSidebar/MobileSidebar.tsx
+++ client/src/components/MobileNavbar/MobileSidebar/MobileSidebar.tsx
@@ -4,22 +4,117 @@ import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import {
+  firstDividerList,
+  secondDividerList,
+  thirdDividerList,
+} from '@/utils/mobileSidebarCategories';
+import { IoIosArrowForward } from 'react-icons/io';
+import ChangeLanguage from '@/components/Navbar/DropDownMenu/ChangeLanguage/ChangeLanguage';
+
+const style = {
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'start',
+  ':hover': {
+    background: '#EDE5FA',
+  },
+};
 
 const MobileSidebar = ({ showSidebar, setShowSidebar }) => {
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation">
+    <Box sx={{ width: 250, fontSize: '14px' }} role="presentation">
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {firstDividerList.map((text) => (
           <ListItem key={text} disablePadding>
-            <ListItemButton></ListItemButton>
+            <ListItemButton
+              sx={{
+                width: '100%',
+                margin: '1px',
+                color: '#6d28d2',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'start',
+                ':hover': {
+                  background: '#EDE5FA',
+                },
+              }}
+            >
+              <div className="flex w-full items-center justify-between">
+                <p>{text}</p>
+              </div>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider sx={{ border: '1px solid gray-100' }} />
+      <List>
+        <ListItemButton
+          sx={{
+            width: '100%',
+            margin: '1px',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'start',
+            ':hover': {
+              background: '#EDE5FA',
+            },
+          }}
+        >
+          Certification preparation <IoIosArrowForward />
+        </ListItemButton>
+      </List>
+      <Divider />
+      <List>
+        <List sx={{ width: '100%', marginLeft: '25px', fontWeight: 700, color: 'gray' }}>
+          Most popular
+        </List>
+        {secondDividerList.map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton
+              sx={{
+                width: '100%',
+                margin: '1px',
+                overflow: 'unset',
+                borderRadius: '5px',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'start',
+                ':hover': {
+                  background: '#EDE5FA',
+                },
+              }}
+            >
+              <p>{text}</p>
+              <IoIosArrowForward />
+            </ListItemButton>
           </ListItem>
         ))}
       </List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        <List sx={{ width: '100%', marginLeft: '25px', fontWeight: 700, color: 'gray' }}>
+          More from Udemy
+        </List>
+        {thirdDividerList.map((text) => (
           <ListItem key={text} disablePadding>
-            <ListItemButton></ListItemButton>
+            <ListItemButton
+              sx={{
+                width: '100%',
+                margin: '1px',
+                overflow: 'unset',
+                borderRadius: '5px',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'start',
+                ':hover': {
+                  background: '#EDE5FA',
+                },
+              }}
+            >
+              {text}
+            </ListItemButton>
           </ListItem>
         ))}
       </List>
@@ -27,7 +122,7 @@ const MobileSidebar = ({ showSidebar, setShowSidebar }) => {
   );
 
   return (
-    <div>
+    <div className="text-black">
       <Drawer open={showSidebar} onClose={() => setShowSidebar(false)}>
         {DrawerList}
       </Drawer>
---
+++ client/src/utils/mobileSidebarCategories.ts
@@ -0,0 +1,22 @@
+export const firstDividerList = ['Login', 'Sign up'];
+
+export const secondDividerList = [
+  'Web Development',
+  'Mobile Development',
+  'Game Development',
+  'Entrepreneurship',
+  'Business Analytics & Intelligence',
+  'Finance',
+  'IT Certification',
+  'Personal Transformation',
+  'Graphic Design & Illustration',
+  'Digital Marketing',
+  'All Categories',
+];
+
+export const thirdDividerList = [
+  'Udemy Business',
+  'Get the app',
+  'Invite friends',
+  'Help and Support',
+];
