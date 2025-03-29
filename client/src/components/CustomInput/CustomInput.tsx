09d9db24e79a985f88718b47f155f8ed4d95d0dd
Yosef Hayim <yosefisabag+03@gmail.com>
2025-03-29 14:40:55 +0300

    Refactor LoginForm component to use CustomInput for improved email input handling and error display

---
+++ client/src/components/CustomInput/CustomInput.tsx
@@ -0,0 +1,67 @@
+import { TextField } from '@mui/material';
+import React, { useRef, useState } from 'react';
+import { PiWarningOctagon } from 'react-icons/pi';
+
+const CustomInput = ({ isError }) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const focusOrBlurRef = useRef(null);
+
+  return (
+    <div className="flex w-full flex-col items-start justify-center gap-2">
+      <div className="relative flex w-full">
+        <TextField
+          ref={focusOrBlurRef}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          inputMode="email"
+          id="email"
+          label="Email"
+          variant="filled"
+          name="email"
+          error={isError}
+          sx={{
+            width: '100%',
+            '& .MuiInputBase-root': {
+              backgroundColor: 'white',
+              border: '1px solid gray',
+              borderRadius: '5px',
+              transition: 'border-color 0.2s ease-in-out',
+              '&:hover': {
+                borderColor: 'purple',
+              },
+              '&.Mui-focused': {
+                borderColor: 'purple',
+                backgroundColor: 'white',
+              },
+              '& input': {
+                color: 'black',
+              },
+            },
+            '& .MuiInputLabel-root': {
+              color: `${isError ? 'red' : 'black'}`,
+              fontWeight: 600,
+              fontSize: 15,
+              '&.Mui-focused': {
+                color: isError ? 'red' : 'black',
+              },
+            },
+          }}
+          slotProps={{
+            input: {
+              disableUnderline: true,
+            },
+          }}
+        />
+        {isError && (
+          <PiWarningOctagon
+            size={18}
+            className={`${isFocused ? '' : 'left-14 top-[28%]'} absolute left-12 top-[4px] text-red-600`}
+          />
+        )}
+      </div>
+      {isError && <p className="text-red-600">Please enter a valid email address.</p>}
+    </div>
+  );
+};
+
+export default CustomInput;
--- client/src/pages/Login/LoginForm/LoginForm.tsx
+++ client/src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,20 +1,17 @@
 import loginUser from '@/api/users/loginUser';
 import Loader from '@/components/Loader/Loader';
 import { emailContext } from '@/routes/AppRoutes';
-import { inputLoginWEmail, regFullButtonPurpleHover } from '@/utils/stylesStorage';
+import { regFullButtonPurpleHover } from '@/utils/stylesStorage';
 import { useMutation } from '@tanstack/react-query';
-import { useContext, useRef, useState } from 'react';
+import { useContext, useState } from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
-import { PiWarningOctagon } from 'react-icons/pi';
-import { TextField } from '@mui/material';
+import CustomInput from '@/components/CustomInput/customInput';
 
 const LoginForm = () => {
   const emailCtx = useContext(emailContext);
   if (!emailCtx) throw new Error('emailContext is not provided');
   const [emailUser, setEmailUser, userFullName, setUserFullName] = emailCtx;
-  const [isFocused, setIsFocused] = useState(false);
-  const inputRef = useRef();
   const [isLoading, setLoading] = useState(false);
   const [isError, setShowIsError] = useState(false);
   const navigate = useNavigate();
@@ -51,57 +48,7 @@ const LoginForm = () => {
   return (
     <form className="mb-4 flex flex-col space-y-4" onSubmit={handleSubmit}>
       <div className="relative">
-        <div className="flex w-full flex-col items-start justify-center gap-2">
-          <div className="relative flex w-full">
-            <TextField
-              ref={inputRef}
-              onFocus={() => setIsFocused(true)}
-              onBlur={() => setIsFocused(false)}
-              inputMode="email"
-              id="email"
-              label="Email"
-              variant="filled"
-              name="email"
-              error={isError}
-              sx={{
-                width: '100%',
-                '& .MuiInputBase-root': {
-                  backgroundColor: 'white',
-                  border: '1px solid gray',
-                  borderRadius: '5px',
-                  transition: 'border-color 0.2s ease-in-out',
-                  '&:hover': {
-                    borderColor: 'purple',
-                  },
-                  '&.Mui-focused': {
-                    borderColor: 'purple',
-                    backgroundColor: 'white',
-                  },
-                  '& input': {
-                    color: 'black',
-                  },
-                },
-                '& .MuiInputLabel-root': {
-                  color: `${isError ? 'red' : 'black'}`,
-                  fontWeight: 600,
-                  fontSize: 15,
-                },
-              }}
-              slotProps={{
-                input: {
-                  disableUnderline: true,
-                },
-              }}
-            />
-            {isError && (
-              <PiWarningOctagon
-                size={18}
-                className={`${isFocused ? '' : 'left-14 top-[28%]'} absolute left-12 top-[4px] text-red-600`}
-              />
-            )}
-          </div>
-          {isError && <p className="text-red-600">Please enter a valid email address.</p>}
-        </div>
+        <CustomInput isError={isError} />
       </div>
       <div>
         <button type="submit" className={`${regFullButtonPurpleHover} w-full`}>
