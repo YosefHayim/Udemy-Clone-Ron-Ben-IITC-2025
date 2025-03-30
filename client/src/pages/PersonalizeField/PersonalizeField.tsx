e0e93545c811c3bea21e4d480ed41a72e70ffde6
yosefhayim <yosefisabag+03@gmail.com>
2025-03-30 21:03:37 +0300

    dsa

--- client/src/pages/PersonalizeField/PersonalizeField.tsx
+++ client/src/pages/PersonalizeField/PersonalizeField.tsx
@@ -1,11 +1,11 @@
 import NavbarPersonalized from "./NavbarPersonalized/NavbarPersonalized";
 import FooterPersonalized from "./FooterPersonalized/FooterPersonalized";
 import { useContext, useEffect } from "react";
-import { personalizeContent } from "@/routes/AppRoutes";
 import PersonalizeFieldP1 from "./PersonalizeFieldP1/PersonalizeFieldP1";
 import OccupationP2 from "./OccupationP2/OccupationP2";
 import SkillsP3 from "./SkillsP3/SkillsP3";
 import CertificationsP4 from "./CertificationsP4/CertificationsP4";
+import { personalizeContent } from "@/contexts/PersonalizeContext";
 
 const PersonalizeField = () => {
   const personalizeTracking = useContext(personalizeContent);
