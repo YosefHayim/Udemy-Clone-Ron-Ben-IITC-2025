ed8532ff52363a8899824d1c542f6f3836557040
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-01 00:28:58 +0300

    fds

--- client/src/utils/banners.tsx
+++ client/src/utils/banners.tsx
@@ -8,6 +8,7 @@ import newsletterBanner from "/images/banner8.jpg";
 import itCertification from "/images/banner.jpg";
 import starYellowBanner from "/images/banner10.jpg";
 import twentyFourHourSale from "/images/banner11.png";
+import cravingSomeFlexibility from "/images/banner12.jpg";
 import { Link } from "react-router-dom";
 
 export const getBanners = ({
@@ -75,6 +76,63 @@ export const getBanners = ({
         </div>
       ),
     },
+    {
+      src: newsletterBanner,
+      content: () => (
+        <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
+          <h1 className="w-full font-extrabold">Benefit big-time</h1>
+          <p className="w-5/6 pr-2 font-sans text-sm">
+            <Link
+              to="/user/edit-notification-preferences/"
+              className="font-medium text-purple-800 underline"
+            >
+              Join our email list
+            </Link>{" "}
+            for special offers, personalized course recommendations, and more.
+          </p>
+        </div>
+      ),
+    },
+    {
+      src: starYellowBanner,
+      content: () => (
+        <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
+          <h1 className="w-full font-extrabold">Every day a little closer</h1>
+          <p className="w-5/6 pr-2 font-sans text-sm">
+            Learning helps you reach your goals.
+            <Link to="/wishlist" className="font-medium text-purple-800 underline">
+              Keep learning
+            </Link>
+            and reap the rewards.
+          </p>
+        </div>
+      ),
+    },
+    {
+      src: twentyFourHourSale,
+      content: () => (
+        <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
+          <h1 className="w-full font-extrabold">24-Hour Flash Sale</h1>
+          <p className="w-5/6 pr-2 font-sans text-sm">
+            Learn from real-world experts for as low as ₪49.90 Sale ends tonight!
+          </p>
+        </div>
+      ),
+    },
+    {
+      src: cravingSomeFlexibility,
+      content: () => (
+        <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
+          <h1 className="w-[300px] font-extrabold">Craving some flexibility?</h1>
+          <div className="flex w-5/6 flex-col items-start justify-start gap-2 pr-2 font-sans text-sm">
+            <p>Explore thousands of highly-rated courses with Personal Plan.</p>
+            <button className="w-min-max rounded-[0.3em] bg-btnColor  px-2 py-2 font-sans font-extrabold text-white hover:bg-btnHoverColor focus:outline-none">
+              Start free week
+            </button>
+          </div>
+        </div>
+      ),
+    },
   ];
 
   if (coursesBoughtOrJoined.length > 1) {
@@ -113,68 +171,23 @@ export const getBanners = ({
     (new Date().getTime() - new Date(registerAt).getTime()) / (1000 * 60 * 60 * 24) <= 14;
 
   if (isWithin14Days) {
-    banners.push(
-      {
-        src: subscribeToTheBestOfUdemy,
-        content: () => (
-          <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
-            <h1 className="w-full font-extrabold">Subscribe to the best of Udemy</h1>
-            <p className="w-[360px] pr-2 font-sans text-sm">
-              With Personal Plan, you get access to 11,000+ of our top-rated courses in tech,
-              business, and more.
-            </p>
-            <div>
-              <button className="w-full rounded-[0.3em] bg-btnColor px-3 py-2 font-sans font-extrabold text-white hover:bg-btnHoverColor focus:outline-none">
-                Try it free
-              </button>
-            </div>
+    banners.push({
+      src: subscribeToTheBestOfUdemy,
+      content: () => (
+        <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
+          <h1 className="w-full font-extrabold">Subscribe to the best of Udemy</h1>
+          <p className="w-[360px] pr-2 font-sans text-sm">
+            With Personal Plan, you get access to 11,000+ of our top-rated courses in tech,
+            business, and more.
+          </p>
+          <div>
+            <button className="w-full rounded-[0.3em] bg-btnColor px-3 py-2 font-sans font-extrabold text-white hover:bg-btnHoverColor focus:outline-none">
+              Try it free
+            </button>
           </div>
-        ),
-      },
-      {
-        src: newsletterBanner,
-        content: () => (
-          <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
-            <h1 className="w-full font-extrabold">Benefit big-time</h1>
-            <p className="w-5/6 pr-2 font-sans text-sm">
-              <Link
-                to="/user/edit-notification-preferences/"
-                className="font-medium text-purple-800 underline"
-              >
-                Join our email list
-              </Link>{" "}
-              for special offers, personalized course recommendations, and more.
-            </p>
-          </div>
-        ),
-      },
-      {
-        src: starYellowBanner,
-        content: () => (
-          <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
-            <h1 className="w-full font-extrabold">Every day a little closer</h1>
-            <p className="w-5/6 pr-2 font-sans text-sm">
-              Learning helps you reach your goals.
-              <Link to="/wishlist" className="font-medium text-purple-800 underline">
-                Keep learning
-              </Link>
-              and reap the rewards.
-            </p>
-          </div>
-        ),
-      },
-      {
-        src: twentyFourHourSale,
-        content: () => (
-          <div className="absolute left-16 top-16 flex w-[450px] flex-col items-start gap-3 rounded-sm border-gray-100 bg-white px-5 py-7 text-black shadow-alertAlgoInfo">
-            <h1 className="w-full font-extrabold">24-Hour Flash Sale</h1>
-            <p className="w-5/6 pr-2 font-sans text-sm">
-              Learn from real-world experts for as low as ₪49.90 Sale ends tonight!
-            </p>
-          </div>
-        ),
-      }
-    );
+        </div>
+      ),
+    });
   }
 
   return banners;
