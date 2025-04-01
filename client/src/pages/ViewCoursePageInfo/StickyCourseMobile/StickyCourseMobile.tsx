b1b79476eb54aeef739223bb11b6e47fbb10b349
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-01 05:18:45 +0300

    hgf

--- client/src/pages/ViewCoursePageInfo/StickyCourseMobile/StickyCourseMobile.tsx
+++ client/src/pages/ViewCoursePageInfo/StickyCourseMobile/StickyCourseMobile.tsx
@@ -23,18 +23,28 @@ const StickyCourseMobile = () => {
   };
 
   return (
-    <div
-      onClick={handleAndNavigateMobile}
-      className="fixed left-0 top-0 z-[1000] flex w-full items-center justify-around bg-white shadow-alertAlgoInfo"
-    >
-      <div className="flex w-full items-center justify-around">
-        {stickyNavbarTopicsMobile.map((mobileTopic) => (
-          <div className={`${selectedButton === mobileTopic && "bg-purple-100"}`}>
-            <button className={`${btnStyleNHover} px-[0.2em] py-[1em]`}>
-              <b className="text-purple-700">{mobileTopic}</b>
-            </button>
-          </div>
-        ))}
+    <div className="w-full">
+      <div
+        onClick={handleAndNavigateMobile}
+        className="fixed left-0 top-0 z-[1000] flex w-full items-center justify-around bg-white shadow-alertAlgoInfo"
+      >
+        <div className="flex w-full items-center justify-around">
+          {stickyNavbarTopicsMobile.map((mobileTopic) => (
+            <div className={`${selectedButton === mobileTopic && "bg-purple-100"}`}>
+              <button className={`${btnStyleNHover} px-[0.2em] py-[1em]`}>
+                <b className="text-purple-700">{mobileTopic}</b>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="fixed bottom-0 left-0 z-[1000] flex w-full flex-col items-center justify-center gap-2 bg-white p-2 px-4">
+        <button className="w-full rounded-[0.3em] bg-btnColor p-3 font-sans text-base font-extrabold text-white hover:bg-btnHoverColor focus:outline-none">
+          Try Personal Plan for free
+        </button>
+        <div className="w-full text-center">
+          <p>Starting at ₪66.67 per month after trial. Cancel anytime.</p>
+        </div>
       </div>
     </div>
   );
