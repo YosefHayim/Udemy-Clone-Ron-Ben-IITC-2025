9b3bff43ed09b42cbe422abe08aae14366fe8934
Ben Kilinski <ben.kilinski@gmail.com>
2025-04-01 21:10:12 +0300

    aba

--- client/src/pages/Home/LearnersAreViewing/LearnersAreViewing.tsx
+++ client/src/pages/Home/LearnersAreViewing/LearnersAreViewing.tsx
@@ -5,11 +5,15 @@ import HomeCourseCard from "@/components/HomeCourseCard/HomeCourseCard";
 import ButtonsCarousel from "@/components/ButtonsCarousel/ButtonsCarousel";
 import Loader from "@/components/Loader/Loader";
 import { filterContext } from "@/contexts/filterSearch";
+import CourseHoverCardInfo from "@/pages/Search/CourseHoverCardInfo/CourseHoverCardInfo";
 
 const LearnersAreViewing = ({ randomAlgoWord }) => {
   const [courseIndex, setCourseIndex] = useState(0);
   const [isCourseAnimating, setCourseAnimating] = useState(false);
   const [countCourseClick, setCourseClick] = useState(0);
+
+  const [hoveredCourse, setHoveredCourse] = useState(null);
+  const [hoverPosition, setHoverPosition] = useState({ top: 0, left: 0 });
 
   const { filterData, setSortBy } = useContext(filterContext);
 
@@ -55,6 +59,7 @@ const LearnersAreViewing = ({ randomAlgoWord }) => {
             rightPosition="2%"
           />
         )}
+
         <div
           className={`flex ${
             data?.response && data?.response?.length > 7
@@ -65,9 +70,47 @@ const LearnersAreViewing = ({ randomAlgoWord }) => {
         >
           {data?.response?.length > 1 &&
             data.response.map((courseCard, index: number) => (
-              <HomeCourseCard courseCard={courseCard} index={index} key={courseCard?._id} />
+              <HomeCourseCard
+                courseCard={courseCard}
+                index={index}
+                key={courseCard?._id}
+                onHover={setHoveredCourse}
+                onPosition={setHoverPosition}
+              />
             ))}
         </div>
+
+        {hoveredCourse && (
+          <div
+            className="absolute z-[1000]"
+            style={{
+              top: `${hoverPosition.top - hoverPosition.top / 20}px`,
+              left: `${hoverPosition.left}px`,
+            }}
+          >
+            <CourseHoverCardInfo
+              courseName={hoveredCourse.courseName}
+              courseLanguages={hoveredCourse.courseLanguages}
+              courseTag={hoveredCourse.courseTag}
+              showCourseLength={true}
+              totalCourseLessons={hoveredCourse.totalCourseLessons}
+              totalCourseDuration={hoveredCourse.totalCourseDuration}
+              courseLevel={hoveredCourse.courseLevel}
+              courseUpdatedAt={hoveredCourse.updatedAt}
+              courseRecapInfo={hoveredCourse.courseRecapInfo}
+              positionedRight={true}
+              width="330px"
+              instructorId={hoveredCourse.courseInstructor?._id}
+              courseTopic={hoveredCourse.courseTopic}
+              index={hoveredCourse._id}
+              displayWhatYouLearn={false}
+              whatYouWillLearn={hoveredCourse.whatYouWillLearn}
+              courseId={hoveredCourse._id}
+              fullPriceCourse={hoveredCourse.courseFullPrice}
+              coursePrice={hoveredCourse.courseDiscountPrice}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
