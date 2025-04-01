b63ba0c181c455f7f7798984c5ca461762a7eae7
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-01 03:40:26 +0300

    fdsfs

--- client/src/contexts/socket.tsx
+++ client/src/contexts/socket.tsx
@@ -4,12 +4,15 @@ import { baseUrl, isProduction, localhostUrl } from "../api/configuration";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { clearUser } from "@/redux/slices/userSlice";
 
 export const SocketContext = createContext(null);
 export const useSocket = () => useContext(SocketContext);
 
 export const SocketProvider = ({ children }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const socketRef = useRef(null);
   const emailOfUser = useSelector((state: RootState) => state?.user?.email);
 
@@ -34,7 +37,9 @@ export const SocketProvider = ({ children }) => {
     socketInstance.on("connect_error", (err) => {
       console.log("Connection failed:", err.data.status);
       if (err.data.status === 401) {
-        navigate("/login");
+        dispatch(clearUser());
+        navigate("/signup");
+        console.log("Your account has been deleted or not created please log in.");
       }
     });
 
