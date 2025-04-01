a45d9edf4e9647847c18225bd6c501d5e29cbfc3
Ben Kilinski <ben.kilinski@gmail.com>
2025-04-01 03:54:26 +0300

    close account

--- client/src/pages/ProfilePage/SwitchPagesProfile/CloseAccount.tsx
+++ client/src/pages/ProfilePage/SwitchPagesProfile/CloseAccount.tsx
@@ -2,14 +2,14 @@ import { Button } from "@/components/ui/button";
 
 const CloseAccount = () => {
   return (
-    <div className="p-[3em]">
+    <div className="min-h-screen border-l">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="font-sans text-[1.3em] font-extrabold">Close Account</h1>
-        <p className="text-[1.2em]">Close your account permanently.</p>
-        <hr className="my-[1em] w-full" />
+        <h1 className="font-sans text-[1.4rem] pt-4 pb-2 font-extrabold">Close Account</h1>
+        <p className="text-[1rem] opacity-90">Close your account permanently.</p>
       </div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="w-[500px] text-start">
+        <hr className="my-[1em] w-full" />
+      <div className="flex flex-col items-center text-[1rem] justify-center mt-8">
+        <div className="w-[600px] text-start">
           <p className="mb-[1em]">
             <b className="text-[#d51c0f]">Warning:</b> If you close your account, you will be
             unsubscribed from all 0 of your courses and will lose access to your account and data
@@ -21,8 +21,8 @@ const CloseAccount = () => {
             you will have 14 days after the initial submission date to reach out to
             privacy@udemy.com to cancel this request.
           </p>
-          <div className="mt-[1em]">
-            <Button className="mr-4 rounded-[0.2em] bg-btnColor px-[1em] py-[1.5em] font-sans font-extrabold text-white shadow-md hover:bg-[#892de1] focus:outline-none focus:outline-none focus:ring-2 focus:ring-purple-500">
+          <div className="mt-[2em]">
+            <Button className="mr-4 rounded-[0.2em] bg-btnColor px-[1em] py-[1.5em] font-sans font-bold text-white shadow-md hover:bg-[#892de1] focus:outline-none focus:outline-none focus:ring-2 focus:ring-purple-500">
               Close account
             </Button>
           </div>
