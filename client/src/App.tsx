9c4ced7ebcd9bf33a4b974801615af273495d6c3
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-01 03:06:33 +0300

    das

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,19 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SocketContext, socket } from "@/contexts/socket";
 import { EmailProvider } from "@/contexts/EmailContext";
 import { PersonalizeProvider } from "@/contexts/PersonalizeContext";
 import AppRoutes from "@/routes/AppRoutes";
 import { FilterProvider } from "./contexts/filterSearch";
-import { RootState } from "./redux/store";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { initialOptions } from "./contexts/PaypalContext";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { SocketProvider } from "./contexts/socket";
 
 export const googleClient = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const queryClient = new QueryClient();
 
 const App = () => {
-  const username = useSelector((state: RootState) => state.user.fullName);
-
-  useEffect(() => {
-    socket.connect();
-
-    socket.on("connect", () => {
-      socket.emit("userConnected", { username });
-    });
-
-    socket.on("welcomeToServer", (data) => {
-      console.log(data);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, [username]);
   return (
-    <SocketContext.Provider value={socket}>
+    <SocketProvider>
       <QueryClientProvider client={queryClient}>
         <GoogleOAuthProvider clientId={googleClient}>
           <EmailProvider>
@@ -47,7 +27,7 @@ const App = () => {
           </EmailProvider>
         </GoogleOAuthProvider>
       </QueryClientProvider>
-    </SocketContext.Provider>
+    </SocketProvider>
   );
 };
 
