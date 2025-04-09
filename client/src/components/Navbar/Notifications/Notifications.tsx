15efe2b0e64d303f95c5a14a4d043d8c385b36e8
Ben Kilinski <ben.kilinski@gmail.com>
2025-04-10 02:07:21 +0300

    notifications hover

--- client/src/components/Navbar/Notifications/Notifications.tsx
+++ client/src/components/Navbar/Notifications/Notifications.tsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import HoverNotification from "./HoverNotification/HoverNotification";
 import { btnStyleNHover } from "@/utils/stylesStorage";
 
 const Notifications = () => {
   const [showNotificationHover, setShowNotificationHover] = useState(false);
+  const hideTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  const handleMouseEnter = () => setShowNotificationHover(true);
-  const handleMouseLeave = () => setShowNotificationHover(false);
+  const handleMouseEnter = () => {
+    if (hideTimeoutRef.current) {
+      clearTimeout(hideTimeoutRef.current);
+    }
+    setShowNotificationHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    hideTimeoutRef.current = setTimeout(() => {
+      setShowNotificationHover(false);
+    }, 200); // delay de 200ms
+  };
 
   return (
-    <div>
-      <div className="relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        <button className={`${btnStyleNHover}`}>
-          <IoMdNotificationsOutline className="h-5 w-5" />
-        </button>
-      </div>
+    <div className="relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <button className={`${btnStyleNHover}`}>
+        <IoMdNotificationsOutline className="h-5 w-5" />
+      </button>
       {showNotificationHover && (
-        <div className="absolute z-[5000]">
+        <div
+          className="absolute right-0 top-[2.5rem] z-[5000]"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           <HoverNotification />
         </div>
       )}
