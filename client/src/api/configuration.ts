b13529284213150c24396555783c033ebb4c745a
yosefhayim <yosefisabag+03@gmail.com>
2025-04-10 01:51:13 +0300

    treter

--- client/src/api/configuration.ts
+++ client/src/api/configuration.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = import.meta.env.VITE_API_BASE_URL;
+export const baseUrl = import.meta.env.VITE_BASE_URL;
 export const localhostUrl = import.meta.env.VITE_LOCALHOST;
 export const isProduction = import.meta.env.VITE_NODE_ENV === "production";
 
